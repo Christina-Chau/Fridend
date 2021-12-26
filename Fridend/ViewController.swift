d810c1ddf54332fd453cb3d0440040c510de98db
Christina-Chau <christinachau5@gmail.com>
2021-12-26 17:12:28 -0500

    Initial Commit

---
+++ Fridend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fridend
+//
+//  Created by Christina Chau on 12/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
